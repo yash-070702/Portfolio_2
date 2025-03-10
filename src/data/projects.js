982d4fdc124a4b9fc8420263ba83bbd4c5f6acb5
Yash Kumar Aggarwal <yashaggarwal2002.ya@gmail.com>
2025-03-10 16:39:16 +0530

    add more projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,10 @@
 import StudyNotion from "../assets/projectImage/StudyNotion.png";
 import Voxera from "../assets/projectImage/Voxera.png";
 import KTS from "../assets/projectImage/KTS.jpg";
+import CodeHive from "../assets/projectImage/CodeHive.webp";
+import WeatherApp from "../assets/projectImage/WeatherApp.png";
+import QuizApp from "../assets/projectImage/QuizApp.png";
+
 
 export const projectData=[
     {   Name:"StudyNotion",
@@ -21,5 +25,25 @@ export const projectData=[
         image:KTS,
         url:"https://studynotion-yash-aggarwal.vercel.app/",
         Description:"Voxera is a real-time chat platform designed for seamless. It enables users to send messages, share media, in real-time conversations with a smooth and intuitive interface.....",
+    },
+    {
+        Name:"CodeHive(Building}",
+        SubHeading:"A Knowledge-Sharing Platform for Developers",
+        image:CodeHive,
+        // url:"Your Project URL",
+        Description:"CodeHive is a dynamic Q&A platform designed for seamless knowledge sharing. It enables users to ask questions, provide answers. With features like upvoting, answer acceptance, and real-time interactions...."
     }
+    ,
+    {   Name:"Weather App",
+        SubHeading:"A Weather Predicting Platform",
+        image:WeatherApp,
+        url:"https://yash-070702.github.io/Weather-App/",
+        Description:"It is a dynamic weather prediction platform that provides real-time forecasts, interactive radar maps, and personalized alerts. Users can track temperature changes, precipitation patterns.... "
+    },
+    {   Name:"Quiz Master",
+        SubHeading:"Where Knowledge Meets Fun!",
+        image:QuizApp,
+        url:"https://quizappusingreact-bbu6vxoqz-yash-kumar-aggarwals-projects.vercel.app/",
+        Description:"QuizMaster is an engaging online quiz platform designed to test and enhance knowledge across various subjects through interactive quizzes, real-time scoring, and personalized challenges..... "
+    },
 ]
